fac8d4082b60b4f332125ffdd73961ad886df596
T. Miller <cerebus2@gmail.com>
2017-09-19 18:21:27 -0500

    Minor fixes

--- AWX_script.sh
+++ AWX_script.sh
@@ -4,4 +4,4 @@
 # If installing under pip, adjust path accordingly.
 # Add reclass model top-level directory to -b flag 
 
-/usr/share/reclass/reclass-ansible -b /path/to/reclass-model $@
+reclass-ansible -b . $@
